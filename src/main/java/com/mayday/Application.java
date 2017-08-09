8ad27ed09ea5d807e94ef36271656a1d3fe57017
Ericiiii <30688833+Ericiiii@users.noreply.github.com>
2017-08-09 09:22:09 +0800

    Update Application.java

--- src/main/java/com/mayday/Application.java
+++ src/main/java/com/mayday/Application.java
@@ -51,6 +51,8 @@ public class Application {
 
     public static void main(String [] args){
         SpringApplication.run(Application.class,args);
+        
+        System.out.println("你不是真正的快乐");
 
     }
 
